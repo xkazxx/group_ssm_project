cece7718be25a4a813cef8b0b9f722bb7d5289ef
xkazxx <xkazxx163@163.com>
2019-05-19 14:48:05 +0800

    解决页面无法显示新增删除编辑按钮

--- ssmproject/src/main/java/com/xkazxx/controller/HomeController.java
+++ ssmproject/src/main/java/com/xkazxx/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.xkazxx.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Zachary Zhao
@@ -11,7 +17,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(){
+    public String home(HttpSession httpSession){
+        List list = new ArrayList();
+        //计划模块
+        list.add("order:add");
+        list.add("order:edit");
+        list.add("order:delete");
+        list.add("custom:add");
+        list.add("custom:edit");
+        list.add("custom:delete");
+        list.add("product:add");
+        list.add("product:edit");
+        list.add("product:delete");
+        list.add("task:add");
+        list.add("task:edit");
+        list.add("task:delete");
+        list.add("manufacture:add");
+        list.add("manufacture:edit");
+        list.add("manufacture:delete");
+        list.add("work:add");
+        list.add("work:edit");
+        list.add("work:delete");
+
+        //请写上自己模块名字,在jsp中找到对应的xxx_list.jsp页面
+        httpSession.setAttribute("sysPermissionList",list);
         return "home";
     }
 
